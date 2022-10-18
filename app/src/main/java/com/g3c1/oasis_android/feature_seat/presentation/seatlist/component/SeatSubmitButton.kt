18d4592d86bd0f8ca38e2cdd6a49b72a4028d1ef
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-18 23:51:52 +0900

    📄 :: font 오프젝트 생성

--- app/src/main/java/com/g3c1/oasis_android/feature_seat/presentation/seatlist/component/SeatSubmitButton.kt
+++ app/src/main/java/com/g3c1/oasis_android/feature_seat/presentation/seatlist/component/SeatSubmitButton.kt
@@ -12,7 +12,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.g3c1.oasis_android.ui.theme.Font
 import com.g3c1.oasis_android.ui.theme.Orange
 
 @Composable
@@ -27,6 +29,6 @@ fun SeatSubmitButton(onClick: () -> Unit) {
             ,
         colors = ButtonDefaults.buttonColors(backgroundColor = Orange, contentColor = Color.White)
     ) {
-        Text(text = "자리 선택")
+        Text(text = "자리 선택", fontFamily = Font.pretendard, fontWeight = FontWeight.ExtraBold)
     }
 }
