e42c29b15e544cb581c392165197768e2872e179
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-20 16:08:11 +0900

    ⚠️ :: git pull

--- app/src/main/java/com/g3c1/oasis_android/feature_seat/presentation/seatlist/component/SeatRadioButton.kt
+++ app/src/main/java/com/g3c1/oasis_android/feature_seat/presentation/seatlist/component/SeatRadioButton.kt
@@ -1,12 +1,16 @@
 package com.g3c1.oasis_android.feature_seat.presentation.example.component
 
+import androidx.compose.material.IconToggleButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.tooling.preview.Preview
 
-@Preview
+
 @Composable
 fun SeatRadioButton() {
-    val selectedValue = remember { mutableStateOf("")}
+    val selectedValue = remember { mutableStateOf("") }
+
+    IconToggleButton(checked = selectedValue.value == , onCheckedChange = selectedValue.value ) {
+
+    }
 }
