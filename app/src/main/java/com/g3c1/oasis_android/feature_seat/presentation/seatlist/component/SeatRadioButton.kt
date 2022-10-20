a150ea2307c169d30064f169238713c8fd9e38ec
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-20 22:28:02 +0900

    ⚰ :: 사용하지 않은 import 제거

--- app/src/main/java/com/g3c1/oasis_android/feature_seat/presentation/seatlist/component/SeatRadioButton.kt
+++ app/src/main/java/com/g3c1/oasis_android/feature_seat/presentation/seatlist/component/SeatRadioButton.kt
@@ -2,7 +2,6 @@ package com.g3c1.oasis_android.feature_seat.presentation.example.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.RadioButton
-import androidx.compose.material.RadioButtonColors
 import androidx.compose.material.RadioButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -10,7 +9,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import com.g3c1.oasis_android.feature_seat.data.data_soure.SeatData
 import com.g3c1.oasis_android.ui.theme.Orange
-import com.g3c1.oasis_android.ui.theme.Purple200
 
 
 @Composable
