99bbbf9b57ea51d1f40f87b8ab19cc2886ba5d8a
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-22 17:10:52 +0900

    📄 :: MenuActivity 생성

---
+++ app/src/main/java/com/g3c1/oasis_android/feature_menu/presentation/MenuActivity.kt
@@ -0,0 +1,16 @@
+package com.g3c1.oasis_android.feature_menu.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MenuActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState, )
+        setContent {
+
+        }
+    }
+}
