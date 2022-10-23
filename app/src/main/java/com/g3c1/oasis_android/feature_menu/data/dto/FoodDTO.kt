3ef6cfeed84a825f22f9d0763db8aa8a71224859
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-23 17:32:51 +0900

    ✨ :: 메뉴 받아오는 DTO 생성

---
+++ app/src/main/java/com/g3c1/oasis_android/feature_menu/data/dto/FoodDTO.kt
@@ -0,0 +1,10 @@
+package com.g3c1.oasis_android.feature_menu.data.dto
+
+data class FoodDTO(
+    val id: Int,
+    val name: String,
+    val img: String,
+    val description: String,
+    val servings: Int,
+    val price: Int
+)
---
+++ app/src/main/java/com/g3c1/oasis_android/feature_menu/data/dto/MenuDTO.kt
@@ -0,0 +1,5 @@
+package com.g3c1.oasis_android.feature_menu.data.dto
+
+data class MenuDTO(
+    val body: List<CategoryDTO>
+)
