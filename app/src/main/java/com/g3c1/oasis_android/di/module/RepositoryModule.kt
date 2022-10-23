c1078554a9c46e2ab670204c2a3ebe396e90d412
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-23 19:30:12 +0900

    ✨ :: return 타입 변경

--- app/src/main/java/com/g3c1/oasis_android/di/module/RepositoryModule.kt
+++ app/src/main/java/com/g3c1/oasis_android/di/module/RepositoryModule.kt
@@ -1,9 +1,8 @@
 package com.g3c1.oasis_android.di.module
 
 import com.g3c1.oasis_android.feature_seat.data.data_source_impl.SeatDataSourceImpl
-import com.g3c1.oasis_android.feature_seat.data.data_soure.SeatDataSource
 import com.g3c1.oasis_android.feature_seat.domain.repository.SeatRepository
-import com.g3c1.oasis_android.feature_seat.domain.repository_impl.SeatRepositoryImpl
+import com.g3c1.oasis_android.feature_menu.domain.repository_impl.SeatRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
