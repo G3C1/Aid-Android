6b94d4eb2816035b2098d14fc631c2125b6d6e2d
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-24 17:43:51 +0900

    ✨ :: 컬러 수정

--- app/src/main/java/com/g3c1/oasis_android/ui/theme/Color.kt
+++ app/src/main/java/com/g3c1/oasis_android/ui/theme/Color.kt
@@ -8,8 +8,12 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
 val Orange = Color(0xFFFF794F)
+val Orage2 = Color(0xFFED875B)
+
 val Gray = Color(0xFFD9D9D9)
 val Gray2 = Color(0xFF898989)
 val Gray3 = Color(0xBF2B2B2B)
-
+val Gray4 = Color(0xFF737373)
+val Gray5 = Color(0xFFE7E7E7)
+val Gray6 = Color(0xFF979797)
 val DarkGray = Color(0xFFA1A1A1)
