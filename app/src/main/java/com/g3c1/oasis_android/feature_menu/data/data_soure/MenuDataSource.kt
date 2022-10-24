54c8bc572303451c383a97996f237d0ad258c0e0
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-24 18:00:52 +0900

    ✨ :: dataSource 생성

---
+++ app/src/main/java/com/g3c1/oasis_android/feature_menu/data/data_soure/MenuDataSource.kt
@@ -0,0 +1,9 @@
+package com.g3c1.oasis_android.feature_menu.data.data_soure
+
+import com.g3c1.oasis_android.feature_menu.data.dto.MenuDTO
+import com.g3c1.oasis_android.remote.util.ApiState
+import kotlinx.coroutines.flow.Flow
+
+interface MenuDataSource {
+    suspend fun getMenuList(): Flow<ApiState<List<MenuDTO>>>
+}
