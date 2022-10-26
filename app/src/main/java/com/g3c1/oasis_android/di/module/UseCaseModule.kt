772e3c1bfcb7e3920dd9dbe31d93b9c153167193
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-10-26 20:11:31 +0900

    ✨ :: 카테고리, 메뉴 받기 기능 끝

--- app/src/main/java/com/g3c1/oasis_android/di/module/UseCaseModule.kt
+++ app/src/main/java/com/g3c1/oasis_android/di/module/UseCaseModule.kt
@@ -1,5 +1,7 @@
 package com.g3c1.oasis_android.di.module
 
+import com.g3c1.oasis_android.feature_menu.domain.repository.MenuRepository
+import com.g3c1.oasis_android.feature_menu.domain.use_case.GetMenuListUseCase
 import com.g3c1.oasis_android.feature_seat.domain.repository.SeatRepository
 import com.g3c1.oasis_android.feature_seat.domain.use_case.GetSeatDataUseCase
 import com.g3c1.oasis_android.feature_seat.domain.use_case.PatchSeatDataUseCase
@@ -20,5 +22,9 @@ object UseCaseModule {
     @Provides
     @Singleton
     fun provideFetchUseCase(repository: SeatRepository): PatchSeatDataUseCase = PatchSeatDataUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideGetUseCase(repository: MenuRepository): GetMenuListUseCase = GetMenuListUseCase(repository)
 
 }
