a3e799f2efe9dddbdbbb52979d8f5f8733ea8048
khs3994 <z01039948003@gmail.com>
2022-10-29 01:56:53 +0900

    :memo: :: 에셋 파일 경로 찾는 로직 삭제

--- app/src/main/java/com/g3c1/oasis_android/feature_chat/presentation/util/Utils.kt
+++ app/src/main/java/com/g3c1/oasis_android/feature_chat/presentation/util/Utils.kt
@@ -1,11 +1,7 @@
 package com.g3c1.oasis_android.feature_chat.presentation.util
 
-import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -23,24 +19,5 @@ object Utils {
             formatted = "${formatted.substring(0, 2)} 00:${formatted.substring(6, 8)}"
         }
         return formatted
-    }
-
-    @Throws(IOException::class)
-    fun assetFilePath(context: Context, assetName: String): String? {
-        val file = File(context.filesDir, assetName)
-        if (file.exists() && file.length() > 0) {
-            return file.absolutePath
-        }
-        context.assets.open(assetName).use { `is` ->
-            FileOutputStream(file).use { os ->
-                val buffer = ByteArray(4 * 1024)
-                var read: Int
-                while (`is`.read(buffer).also { read = it } != -1) {
-                    os.write(buffer, 0, read)
-                }
-                os.flush()
-            }
-            return file.absolutePath
-        }
     }
 }
