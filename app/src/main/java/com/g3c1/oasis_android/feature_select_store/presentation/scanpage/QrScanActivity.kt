64826709803537488ff15496347a4228eab9fe1d
khs3994 <z01039948003@gmail.com>
2022-11-09 01:04:56 +0900

    :sparkles: :: 상단 컴포넌트

--- app/src/main/java/com/g3c1/oasis_android/feature_select_store/presentation/scanpage/QrScanActivity.kt
+++ app/src/main/java/com/g3c1/oasis_android/feature_select_store/presentation/scanpage/QrScanActivity.kt
@@ -14,20 +14,24 @@ import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import com.g3c1.oasis_android.R
 import com.g3c1.oasis_android.feature_select_store.presentation.util.QrCodeAnalyer
 
 class QrScanActivity : ComponentActivity() {
@@ -59,51 +63,75 @@ class QrScanActivity : ComponentActivity() {
             LaunchedEffect(key1 = true) {
                 launcher.launch(Manifest.permission.CAMERA)
             }
-            Column(
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
             ) {
-                if (hasCamPermision) {
-                    AndroidView(
-                        factory = { context ->
-                            val previewView = PreviewView(context)
-                            val preview = Preview.Builder().build()
-                            val selector = CameraSelector.Builder()
-                                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                                .build()
-                            preview.setSurfaceProvider(previewView.surfaceProvider)
-                            val imageAnalysis = ImageAnalysis.Builder()
-                                .setTargetResolution(Size(previewView.width, previewView.height))
-                                .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
-                                .build()
-                            imageAnalysis.setAnalyzer(
-                                ContextCompat.getMainExecutor(context),
-                                QrCodeAnalyer { result ->
-                                    code = result
-                                }
-                            )
-                            try {
-                                cameraProviderFuture.get().bindToLifecycle(
-                                    lifecycleOwner,
-                                    selector,
-                                    preview,
-                                    imageAnalysis
+                Box(
+                    contentAlignment = Alignment.Center
+                ) {
+                    if (hasCamPermision) {
+                        AndroidView(
+                            factory = { context ->
+                                val previewView = PreviewView(context)
+                                val preview = Preview.Builder().build()
+                                val selector = CameraSelector.Builder()
+                                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                                    .build()
+                                preview.setSurfaceProvider(previewView.surfaceProvider)
+                                val imageAnalysis = ImageAnalysis.Builder()
+                                    .setTargetResolution(
+                                        Size(
+                                            previewView.width,
+                                            previewView.height
+                                        )
+                                    )
+                                    .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
+                                    .build()
+                                imageAnalysis.setAnalyzer(
+                                    ContextCompat.getMainExecutor(context),
+                                    QrCodeAnalyer { result ->
+                                        code = result
+                                    }
                                 )
-                            } catch (e: Exception) {
-                                e.printStackTrace()
-                            }
-                            previewView
-                        },
-                        modifier = Modifier.weight(1f)
-                    )
-                    Text(
-                        text = code,
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(32.dp)
-                    )
+                                try {
+                                    cameraProviderFuture.get().bindToLifecycle(
+                                        lifecycleOwner,
+                                        selector,
+                                        preview,
+                                        imageAnalysis
+                                    )
+                                } catch (e: Exception) {
+                                    e.printStackTrace()
+                                }
+                                previewView
+                            },
+                            modifier = Modifier.fillMaxSize()
+                        )
+                        Image(
+                            painterResource(id = R.drawable.qr_frame),
+                            contentDescription = "QRcodeFrame"
+                        )
+                    }
+                }
+                Box(
+                    contentAlignment = Alignment.TopCenter,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp)
+                ) {
+                    if (code != "") {
+                        Row(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(10.dp))
+                                .shadow(10.dp)
+                                .fillMaxWidth(0.95f)
+                                .fillMaxHeight(0.15f)
+                                .background(Color.White)
+                        ) {
+                            Text(text = code)
+                        }
+                    }
                 }
             }
         }
