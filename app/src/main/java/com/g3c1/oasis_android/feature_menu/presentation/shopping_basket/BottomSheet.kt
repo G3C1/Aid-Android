5e9e0f753eba35eb63474035d6a08ad8c5922f01
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-11-11 10:13:01 +0900

    ✨ :: 리스트에 갑싱 있는지 조건문확인, 버튼 생성

--- app/src/main/java/com/g3c1/oasis_android/feature_menu/presentation/shopping_basket/BottomSheet.kt
+++ app/src/main/java/com/g3c1/oasis_android/feature_menu/presentation/shopping_basket/BottomSheet.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.unit.sp
 import com.g3c1.oasis_android.R
 import com.g3c1.oasis_android.feature_menu.presentation.menu.component.TopBar
 import com.g3c1.oasis_android.feature_menu.presentation.menu.navigation.MenuNavigation
+import com.g3c1.oasis_android.feature_menu.presentation.shopping_basket.component.OrderButton
 import com.g3c1.oasis_android.feature_menu.presentation.shopping_basket.component.OrderedMenuComponent
 import com.g3c1.oasis_android.feature_menu.presentation.vm.MenuViewModel
 import com.g3c1.oasis_android.ui.theme.Font
@@ -44,49 +45,57 @@ fun BottomSheet(viewModel: MenuViewModel) {
         sheetContent = {
             val orderMenuList = remember { viewModel.orderMenuList }
             Log.d("TAG", "BottomSheet: orderMenuList = $orderMenuList")
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Spacer(modifier = Modifier.height(30.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.receipt),
-                    contentDescription = "receipt image",
-                    modifier = Modifier
-                        .width(100.dp)
-                        .height(90.dp)
-                )
-                Text(
-                    text = "선택하신 메뉴들이에요.",
-                    fontFamily = Font.pretendard,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold
-                )
-                Spacer(modifier = Modifier.height(66.dp))
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(72.dp),
+            Box(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    items(orderMenuList.size) {
-                        OrderedMenuComponent(
-                            data = orderMenuList[it],
-                            viewModel = viewModel
+                    Spacer(modifier = Modifier.height(30.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.receipt),
+                        contentDescription = "receipt image",
+                        modifier = Modifier
+                            .width(100.dp)
+                            .height(90.dp)
+                    )
+                    Text(
+                        text = "선택하신 메뉴들이에요.",
+                        fontFamily = Font.pretendard,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.height(66.dp))
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(72.dp),
+                    ) {
+                        items(orderMenuList.size) {
+                            OrderedMenuComponent(
+                                data = orderMenuList[it],
+                                viewModel = viewModel
+                            )
+                        }
+                    }
+                    Canvas(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                    ) {
+                        drawRect(
+                            color = Gray5,
+                            size = size
                         )
                     }
                 }
-                Canvas(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                ) {
-                    drawRect(
-                        color = Gray5,
-                        size = size
-                    )
-                }
-
+                OrderButton(
+                    price = viewModel.orderMenuList.sumOf { it.amount * it.price },
+                    visibility = viewModel.orderMenuList.isNotEmpty(),
+                    onClick = {  },
+                    modifier = Modifier.align(Alignment.BottomCenter)
+                )
             }
+
         },
         sheetPeekHeight = 0.dp
     ) {
--- app/src/main/java/com/g3c1/oasis_android/feature_menu/presentation/shopping_basket/component/OrderButton.kt
+++ app/src/main/java/com/g3c1/oasis_android/feature_menu/presentation/shopping_basket/component/OrderButton.kt
@@ -1,8 +1,46 @@
 package com.g3c1.oasis_android.feature_menu.presentation.shopping_basket.component
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.g3c1.oasis_android.ui.theme.Font
+import com.g3c1.oasis_android.ui.theme.Orange
 
 @Composable
-fun OrderButton(price: Int) {
-
+fun OrderButton(price: Int, visibility: Boolean, onClick: () -> Unit, modifier: Modifier) {
+    AnimatedVisibility(visible = visibility, modifier = modifier) {
+        Button(
+            onClick = onClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+                .padding(8.dp, 0.dp, 8.dp, 16.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Orange,
+                contentColor = Color.White
+            )
+        ) {
+            Text(
+                text = "주문",
+                fontFamily = Font.pretendard,
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 18.sp
+            )
+            Spacer(modifier = Modifier.width(100.dp))
+            Text(
+                text = "${price}원",
+                fontFamily = Font.pretendard,
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 14.sp
+            )
+        }
+    }
 }
