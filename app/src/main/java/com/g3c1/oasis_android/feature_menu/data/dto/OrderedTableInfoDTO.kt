5ca1bfe54a697a15ab03a8d5d841c0c6bf86b339
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-11-13 16:59:18 +0900

    ✨ :: Data Class, Api 생성

---
+++ app/src/main/java/com/g3c1/oasis_android/feature_menu/data/dto/OrderedTableInfoDTO.kt
@@ -0,0 +1,6 @@
+package com.g3c1.oasis_android.feature_menu.data.dto
+
+data class OrderedTableInfoDTO(
+    val seatId: Int,
+    val foodLists: List<OrderInfoDTO>
+)
---
+++ app/src/main/java/com/g3c1/oasis_android/remote/api/PurchaseApi.kt
@@ -0,0 +1,11 @@
+package com.g3c1.oasis_android.remote.api
+
+import com.g3c1.oasis_android.feature_menu.data.dto.OrderedTableInfoDTO
+import retrofit2.Response
+import retrofit2.http.POST
+
+interface PurchaseApi {
+
+    @POST("purchase/")
+    suspend fun sendsTheOrderedFoodListToTheServer(): Response<OrderedTableInfoDTO>
+}
