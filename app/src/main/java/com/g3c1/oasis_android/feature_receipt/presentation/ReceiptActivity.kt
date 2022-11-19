b4f1c52f642bb235f8c84f86409d6693ffe20055
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-11-19 16:00:15 +0900

    📄 :: ReceiptScreen 생성, DI 설정

--- app/src/main/java/com/g3c1/oasis_android/feature_receipt/presentation/ReceiptActivity.kt
+++ app/src/main/java/com/g3c1/oasis_android/feature_receipt/presentation/ReceiptActivity.kt
@@ -3,12 +3,15 @@ package com.g3c1.oasis_android.feature_receipt.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.g3c1.oasis_android.feature_receipt.presentation.receipt.ReceiptScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ReceiptActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            ReceiptScreen()
         }
     }
 }
---
+++ app/src/main/java/com/g3c1/oasis_android/feature_receipt/presentation/receipt/ReceiptScreen.kt
@@ -0,0 +1,8 @@
+package com.g3c1.oasis_android.feature_receipt.presentation.receipt
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ReceiptScreen() {
+
+}
