335f2d92cf7548d3c3f5b8d0a7f1cd4e3ba708a6
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-11-19 15:51:43 +0900

    ✨ :: Activity 생성

---
+++ app/src/main/java/com/g3c1/oasis_android/feature_receipt/presentation/ReceiptActivity.kt
@@ -0,0 +1,14 @@
+package com.g3c1.oasis_android.feature_receipt.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ReceiptActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
