3b4e3baa8acbfa06b66d795f2cf018ae17501ccf
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-12-28 09:04:26 +0900

    ✨ :: DTO 수정

--- app/src/main/java/com/g3c1/oasis_android/feature_menu/data/dto/OrderedTableInfoDTO.kt
+++ app/src/main/java/com/g3c1/oasis_android/feature_menu/data/dto/OrderedTableInfoDTO.kt
@@ -1,6 +1,7 @@
 package com.g3c1.oasis_android.feature_menu.data.dto
 
 data class OrderedTableInfoDTO(
-    val seatId: Int,
+    val serialNumber: Long,
+    val seatId: Long,
     val foodList: List<OrderInfoDTO>
 )
